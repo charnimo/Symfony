35d743acfb25325bd276c46b7d7c2af843bf26dd
ilyes <ilyesbenjemaa17@gmail.com>
2024-05-27 14:35:53 +0100

    fixing error

--- src/Entity/Products.php
+++ src/Entity/Products.php
@@ -107,15 +107,5 @@ class Products
 
         return $this;
     }
-    public function getSlug(): ?string
-    {
-        return $this->slug;
-    }
-
-    public function setSlug(string $slug): self
-    {
-        $this->slug = $slug;
-
-        return $this;
-    }
+    
 }
