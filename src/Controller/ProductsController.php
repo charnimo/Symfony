7e2c44f8c29fa416713f4469a3fbb75b817cb098
Ahmed <jeffbanarie123@gmail.com>
2024-05-29 00:26:27 +0100

    correct redirect

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -6,14 +6,23 @@ use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ProductsController extends AbstractController
 {
     #[Route('/products', name: 'user_products')]
     public function index(ProductsRepository $productsRepository): Response
     {
+        // Check if the user has the 'ROLE_ADMIN'
+        if ($this->isGranted('ROLE_ADMIN')) {
+            // Redirect to another page if the user is an admin
+            return $this->redirectToRoute('admin_home');
+        }
+
+        // Fetch all products if the user is not an admin
         $products = $productsRepository->findAll();
+
+        // Render the products page
         return $this->render('product/index.html.twig', [
             'products' => $products,
         ]);
